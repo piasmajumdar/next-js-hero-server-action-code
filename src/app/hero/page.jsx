4a7cb9a6da9469910bfc6c10890702f5d476f311
piasmajumdar <piasmajumdar@gmail.com>
2026-04-28 23:57:42 +0530

    first commit

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@heroui/react';
+import React from 'react';
+
+const HeroPage = () => {
+    return (
+        <div>
+            <h2>Hero Button</h2>
+            <Button variant='secondary'>Hero Button</Button>
+            <Button variant='outline'>Hero Button</Button>
+        </div>
+    );
+};
+
+export default HeroPage;
