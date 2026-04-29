ac635cf2386f8ea26703163132d1b9ee1051571f
piasmajumdar <piasmajumdar@gmail.com>
2026-04-29 06:26:56 +0530

    Added modal

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,15 +1,21 @@
+import { AddTask } from '@/components/AddTask';
 import TasksCard from '@/components/TasksCard';
 import { getTasks } from '@/lib/tasks';
 import React from 'react';
 
-const TasksPage = async() => {
+const TasksPage = async () => {
     const tasks = await getTasks();
     return (
         <div>
-            <h2> Tasks: {tasks.length}</h2>
-            {
-                tasks.map(task=> <TasksCard key={task.id} task={task}></TasksCard>)
-            }
+            <h2 className='p-7'> Tasks: {tasks.length}</h2>
+            <div className='ml-7'>
+                <AddTask></AddTask>
+            </div>
+            <div className='p-8'>
+                {
+                    tasks.map(task => <TasksCard key={task.id} task={task}></TasksCard>)
+                }
+            </div>
         </div>
     );
 };
